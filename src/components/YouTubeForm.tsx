ae148c3839ccda2a0a23e996fb2a77106800bda5
Andydiii <Andydiii0813@gmail.com>
2025-03-23 00:29:00 -0400

    Learned how to register the form and check if it is managed by react hook form with DevTool component (need to set control). Learned how to submit the form with handlesubmit(onSubmit) where OnSubmit is the customized submit function, error handling(default and customized)

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -12,9 +12,10 @@ type FormValues = {
 
 export function YouTubeForm() {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit} = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
-  const onSubmit = (data: FormValues) => {
+  const onSubmit = (data: FormValues) => { 
     console.log('Form submitted', data);
   }
 
@@ -22,15 +23,33 @@ export function YouTubeForm() {
   return (
     <div>
       <h1>YouTube Form Count: {renderCount / 2}</h1>
-      <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: 'column', gap: '5px'}}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate style={{display: "flex", flexDirection: 'column', gap: '5px'}}>
         <label htmlFor="username"> Username </label>
-        <input type="text" id="username" {...register("username")}/>
+        <input type="text" id="username" {...register("username", {
+          required: {     // if input exists, no error, else error message
+            value: true,
+            message: "Username is required" // default error handling: if input exists, no error, else error message
+          }
+        })}/>
+        <p className='error'>{errors.username?.message}</p>
 
         <label htmlFor="email"> Email </label>
-        <input type="email" id="email" {...register("email")}/>
+        <input type="email" id="email" {...register("email", {
+          required: "Email is required", // default error handling: if input exists, no error, else errror message.
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, //regex pattern for email
+            message: "Invalid email address" // invalid email error message
+          }
+        })}/>
+        <p className='error'>{errors.email?.message}</p>
+
 
         <label htmlFor="channel"> Channel </label>
-        <input type="text" id="channel" {...register("channel")}/>
+        <input type="text" id="channel" {...register("channel", {
+          required: "Channel is required" // default error handling: if input exists, no error, else error message
+        })}/>
+        <p className='error'>{errors.channel?.message}</p>
+
 
         <button>Submit</button>
       </form>
